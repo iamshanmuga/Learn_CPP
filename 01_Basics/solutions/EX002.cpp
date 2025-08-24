a9bf1d06f8003e171b020bc8489cc52f719b2939
Shanmuga Budda <shanmugabudda@gmail.com>
2025-08-24 09:29:56 -0500

    Refactoring: Solution for basic exercise - EX002.

--- 01_Basics/solutions/EX002.cpp
+++ 01_Basics/solutions/EX002.cpp
@@ -17,16 +17,18 @@ int main() {
     std::cin >> num2; // Read the second integer from user input
 
     // Perform and display the results of various arithmetic operations
-    std::cout << "Sum: " << (num1 + num2) << std::endl;          // Output the sum of num1 and num2
-    std::cout << "Difference: " << (num1 - num2) << std::endl;   // Output the difference of num1 and num2
-    std::cout << "Product: " << (num1 * num2) << std::endl;      // Output the product of num1 and num2
+    std::cout << "Sum: " << (num1 + num2) << std::endl;             // Output the sum of num1 and num2
+    std::cout << "Difference: " << (num1 - num2) << std::endl;      // Output the difference of num1 and num2
+    std::cout << "Product: " << (num1 * num2) << std::endl;         // Output the product of num1 and num2
     // Check for division by zero before performing division
     if (num2 != 0) {
-        std::cout << "Quotient: " << (num1 / num2) << std::endl; // Output the quotient of num1 and num2
+        std::cout << "Quotient: " << (num1 / num2) << std::endl;    // Output the quotient of num1 and num2
+        std::cout << "Remainder: " << (num1 % num2) << std::endl;   // Output the remainder of num1 divided by num2
+
     } else {
-        std::cout << "Error: Division by zero is not allowed." << std::endl; // Handle division by zero
+        std::cout << "Error: Division by zero is not allowed." << std::endl;                    // Handle division by zero
+        std::cout << "Error: Cannot calculate remainder if Second Integer is 0" << std::endl;   // Handle division by zero
     }
-    std::cout << "Remainder: " << (num1 % num2) << std::endl;   // Output the remainder of num1 divided by num2
 
     return 0;
 }
